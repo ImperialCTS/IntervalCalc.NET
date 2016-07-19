94e5ac1ec8e52ed1fb93d15a9c6c12d41a7b194f
Ali Niknejad <aliniknejad@gmail.com>
2016-07-19 18:04:02 +0100

    Revert "Adding web example project"
    This reverts commit 7f9c5d8a2a8f6b3135f623db779b836541a57d53.

--- IntervalCalc.NET/Interval.cs
+++ IntervalCalc.NET/Interval.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,7 +20,6 @@ namespace IntervalCalc
 
         public double Middle => (A + B) / 2;
 
-        [EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
         public double Any
         {
             get
